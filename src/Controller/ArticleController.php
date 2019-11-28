11dd8e72a91e88eaf0e75ceef2dd0e33b7c994fb
Krzysztof <bak1990@o2.pl>
2019-11-28 20:58:05 +0100

    Progress

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,10 +4,11 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -22,6 +23,11 @@ class ArticleController
      */
     public function show($slug)
     {
-        return new Response(sprintf('Strona %s która będzie w przyszłości!!!!!', $slug));
+        $comments = ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at dignissim ex.', ' In tempor eleifend porttitor. Sed nec fermentum ligula. Aenean varius nisl et hendrerit luctus. Pellentesque nisi justo, vehicula a blandit non, fermentum et quam. Cras id libero in sem porttitor dignissim. Duis quam ligula, fringilla sit amet ornare sit amet, suscipit nec turpis. ', 'Pellentesque placerat egestas nunc, ac suscipit tellus blandit quis. Mauris sit amet augue quis libero vehicula tincidunt eu non magna. Duis tempor neque id sollicitudin fermentum. Nunc sed rhoncus nisl.'];
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments,
+        ]);
+//        return new Response(sprintf('Strona %s która będzie w przyszłości!!!!!', $slug));
     }
 }
