e818d1971ee3321e1e40788b67a0abe9080dfee1
Krzysztof <bak1990@o2.pl>
2019-12-03 22:49:46 +0100

    Add simple Ajax counter, first Json endpoint

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,21 +5,23 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/" ,name="app_homepage")
      */
     public function homepage()
     {
-        return new Response('Moja pierwsza strona jest już gotowa!!!!');
+        //return new Response('Moja pierwsza strona jest już gotowa!!!!');
+        return $this->render('article/homepage.html.twig');
     }
 
     /**
-     * @Route("/news/{slug}")
+     * @Route("/news/{slug}", name="article_show")
      */
     public function show($slug)
     {
@@ -27,8 +29,17 @@ class ArticleController extends AbstractController
         $comments = ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at dignissim ex.', ' In tempor eleifend porttitor. Sed nec fermentum ligula. Aenean varius nisl et hendrerit luctus. Pellentesque nisi justo, vehicula a blandit non, fermentum et quam. Cras id libero in sem porttitor dignissim. Duis quam ligula, fringilla sit amet ornare sit amet, suscipit nec turpis. ', 'Pellentesque placerat egestas nunc, ac suscipit tellus blandit quis. Mauris sit amet augue quis libero vehicula tincidunt eu non magna. Duis tempor neque id sollicitudin fermentum. Nunc sed rhoncus nisl.'];
         return $this->render('article/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug)),
+            'slug' => $slug,
             'comments' => $comments,
         ]);
 //        return new Response(sprintf('Strona %s która będzie w przyszłości!!!!!', $slug));
+    }
+
+    /**
+     * @Route("news/{slug}/heart", name="article_toggle_heart",methods={"POST"})
+     */
+    public function toggleArticleHeart($slug)
+    {
+        return new JsonResponse(['hearts' => rand(5, 100)]);
     }
 }
