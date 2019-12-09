cc7aff9f55cc82cfa59fbfdc201f67c6f5c6c56e
Krzysztof <bak1990@o2.pl>
2019-12-09 22:16:15 +0100

    Next steps with Symfony 5 services

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -5,21 +5,28 @@ namespace App\Service;
 
 
 use Michelf\MarkdownInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
 
 class MarkdownHelper
 {
     private $cache;
     private $markdown;
+    private $logger;
 
-    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown)
+    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $logger)
     {
         $this->markdown = $markdown;
         $this->cache = $cache;
+        $this->logger = $logger;
     }
 
     public function parse(string $source): string
     {
+
+        if (stripos($source, 'placerat') != false) {
+            $this->logger->info('Coś tam!!!');
+        }
         $item = $this->cache->getItem('markdown' . md5($source));
         if (!$item->isHit()) {
             $item->set($this->markdown->transform($source));
