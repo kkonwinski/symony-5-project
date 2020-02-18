3f6699df3ca273256d15e438f9aa21c9d96173be
Krzysztof <bak1990@o2.pl>
2020-02-18 20:36:43 +0100

    api token entity

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\ApiToken;
 use App\Entity\User;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -19,7 +20,7 @@ class UserFixture extends BaseFixture
     protected function loadData(ObjectManager $manager)
     {
 
-        $this->createMany(10, 'main_users', function ($i) {
+        $this->createMany(10, 'main_users', function ($i) use ($manager) {
             $user = new User();
             $user->setEmail(sprintf('spacebar%d@example.com', $i));
             $user->setFirstName($this->faker->firstName);
@@ -30,6 +31,10 @@ class UserFixture extends BaseFixture
             $user->setPassword($this->userPasswordEncoder->encodePassword(
                 $user, 'asdf'
             ));
+            $apiToken1 = new ApiToken($user);
+            $apiToken2 = new ApiToken($user);
+            $manager->persist($apiToken1);
+            $manager->persist($apiToken2);
             return $user;
         });
         $this->createMany(3, 'admin_users', function ($i) {
