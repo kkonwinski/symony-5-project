b75b4074875c41156982ec8c262490f01283a3d2
Krzysztof <bak1990@o2.pl>
2020-02-20 14:09:42 +0100

    author relation , custom voters

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -4,23 +4,34 @@
 namespace App\Controller;
 
 
+use App\Entity\Article;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @IsGranted("ROLE_ADMIN_ARTICLE")
- */
+
 class ArticleAdminController extends AbstractController
 {
     /**
      * @Route("/admin/article/new", name="admin_article_new")
+     * @IsGranted("ROLE_ADMIN_ARTICLE")
      */
     public function new(EntityManagerInterface $em)
     {
         die('todo');
         return new Response(sprintf('działa slug %s id: #%d', $article->getSlug(), $article->getId()));
+    }
+
+    /**
+     * @Route("/admin/article/{id}/edit")
+     * @IsGranted("MANAGE",subject="article")
+     */
+    public function edit(Article $article)
+    {
+        //$this->denyAccessUnlessGranted('MANAGE',$article);
+
+        dd($article);
     }
 }
